268041f02aed10f9a198d49fb4ede7eca670ef28
moonlvr <lesliemae.dejayco@gmail.com>
2026-09-06 23:38:18 +0800

    deploy flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "755345701" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2323709021" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
